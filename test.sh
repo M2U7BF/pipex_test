aa9790865d4e494a7782c1fca9219aec870a7ba1
M2U7BF <yhvh4321@gmail.com>
2025-05-07 14:55:18 +0900

    update

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!bin/pipex
 
 # pipex_test 1 "$expected" "infile" "ls" "wc -l" "outfile"
 
@@ -40,19 +40,19 @@ if [ ! -e "infile_permission" ]; then
 fi
 
 # 1~5
-pipex_test 0 "bash: nonexisting: No such file or directory" "nonexisting" "ls" "wc -l" "outfile"
+pipex_test 0 "pipex: nonexisting: No such file or directory" "nonexisting" "ls" "wc -l" "outfile"
 pipex_test 0 "nonexisting: command not found" "infile" "nonexisting" "wc -l" "outfile"
 pipex_test 127 "nonexisting: command not found" "infile" "ls" "nonexisting" "outfile"
-pipex_test 0 "bash: infile_permission: Permission denied" "infile_permission" "ls" "wc -l" "outfile"
-pipex_test 0 "bash: /usr/bin/sudo: Permission denied" "infile" "sudo" "wc -l" "outfile"
+pipex_test 0 "pipex: infile_permission: Permission denied" "infile_permission" "ls" "wc -l" "outfile"
+pipex_test 0 "pipex: /usr/bin/sudo: Permission denied" "infile" "sudo" "wc -l" "outfile"
 # 
-pipex_test 126 "bash: /usr/bin/sudo: Permission denied" "infile" "ls" "sudo" "outfile"
-pipex_test 1 "bash: outfile_permission: Permission denied" "infile" "ls" "wc -l" "outfile_permission"
-pipex_test 0 "bash: : No such file or directory" "" "ls" "wc -l" "outfile"
+pipex_test 126 "pipex: /usr/bin/sudo: Permission denied" "infile" "ls" "sudo" "outfile"
+pipex_test 1 "pipex: outfile_permission: Permission denied" "infile" "ls" "wc -l" "outfile_permission"
+pipex_test 0 "pipex: : No such file or directory" "" "ls" "wc -l" "outfile"
 pipex_test 0 "'' : command not found" "infile" "" "wc -l" "outfile"
 pipex_test 127 "'' : command not found" "infile" "ls" "" "outfile"
 # 
-pipex_test 1 "bash: : No such file or directory" "infile" "ls" "wc -l" ""
+pipex_test 1 "pipex: : No such file or directory" "infile" "ls" "wc -l" ""
 pipex_test 0 "" "infile" "ls -l" "wc" "outfile"
 pipex_test 0 "" "infile" "ls" "wc -l" "outfile"
 pipex_test 0 "" "infile" "/usr/bin/ls" "wc -l" "outfile"
@@ -60,18 +60,18 @@ pipex_test 0 "" "infile" "ls" "/usr/bin/wc -l" "outfile"
 # 
 pipex_test 0 "" "infile" "grep nonexisting" "wc -l" "outfile"
 pipex_test 1 "" "infile" "ls" "grep nonexisting" "outfile"
-pipex_test 0 "bash: /: Is a directory" "infile" "/" "wc -l" "outfile"
-pipex_test 126 "bash: /: Is a directory" "infile" "ls" "/" "outfile"
-pipex_test 0 "bash: .: filename argument required" "infile" "." "wc -l" "outfile"
+pipex_test 0 "pipex: /: Is a directory" "infile" "/" "wc -l" "outfile"
+pipex_test 126 "pipex: /: Is a directory" "infile" "ls" "/" "outfile"
+pipex_test 0 "pipex: .: filename argument required" "infile" "." "wc -l" "outfile"
 # 
-pipex_test 2 "bash: .: filename argument required" "infile" "ls" "." "outfile"
+pipex_test 2 "pipex: .: filename argument required" "infile" "ls" "." "outfile"
 pipex_test 0 "" "./lib" "ls" "wc -l" "outfile"
-pipex_test 1 "bash: ./lib: Is a directory" "infile" "ls" "wc -l" "./lib"
-pipex_test 0 "bash: /usr/bin/lsxx: No such file or directory" "infile" "/usr/bin/lsxx" "wc -l" "outfile"
-pipex_test 127 "bash: /usr/bin/lsxx: No such file or directory" "infile" "ls" "/usr/bin/lsxx" "outfile"
+pipex_test 1 "pipex: ./lib: Is a directory" "infile" "ls" "wc -l" "./lib"
+pipex_test 0 "pipex: /usr/bin/lsxx: No such file or directory" "infile" "/usr/bin/lsxx" "wc -l" "outfile"
+pipex_test 127 "pipex: /usr/bin/lsxx: No such file or directory" "infile" "ls" "/usr/bin/lsxx" "outfile"
 # 
 rm -f " "
-pipex_test 0 "bash:  : No such file or directory" " " "ls -l" "wc -l" "outfile"
+pipex_test 0 "pipex:  : No such file or directory" " " "ls -l" "wc -l" "outfile"
 pipex_test 0 " : command not found" "infile" " " "wc -l" "outfile"
 pipex_test 127 " : command not found" "infile" "ls -l" " " "outfile"
 pipex_test 0 "" "infile" "ls -l" "wc -l" " "
